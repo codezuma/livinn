9faa72dc0e410de2973f68959142f42bab5162c0
chandresh <patidarchandresh2002@gmail.com>
2022-05-29 23:18:55 +0530

    second commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'nav',component:NavComponent},
+  
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabledBlocking'
+})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
